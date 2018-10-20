5536ec0ca6e838e05d40fe8353f7f993f9544d37
tanisha98x <42790950+tanisha98x@users.noreply.github.com>
2018-10-19 20:16:07 -0400

    Update EfficientWordMarkov.java

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -1,71 +1,68 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
-import java.util.Random;
+import java.util.*;
+public class EfficientMarkov extends BaseMarkov{
+	private HashMap<String,ArrayList<String>> myMap;
 
-public class EfficientWordMarkov extends BaseWordMarkov {
-	private HashMap<WordGram,ArrayList<String>> myMap;
-	
+	/**
+	 * Construct a EfficientMarkov object with
+	 * the specified order
+	 * @param order size of this markov generator
+	 */
+	public EfficientMarkov(int order) {
+		super(order);//
+		myMap = new HashMap<String, ArrayList<String>>();
+	}
+
 	/**
 	 * Default constructor has order 3
 	 */
-	public EfficientWordMarkov() {
-		this(2);
+	public EfficientMarkov() {
+		this(3);
 	}
+	
 	/**
-	 * Construct a EfficientWordMarkov object with
-	 * the specified order
-	 * @param order size of this Markov generator
-	 */
-	public EfficientWordMarkov(int order){
-		super(order); //
-		myMap = new HashMap<WordGram, ArrayList<String>>();
-	}
-	/**
-	 * Creates a map with keys of that are wordgrams of length my order from the given text and values that are array lists of 
-	 * strings of words that follow the key. 
+	 * Creates a map with keys of that are strings of length my order from the given text and values that are array lists of 
+	 * strings of characters that follow the key. At the end of the text, a Pseudo Eos tag is added that breaks the loop
 	 * @param a given string
 	 */
 	@Override
-	public void setTraining(String text){
+	public void setTraining(String text) {
 		myMap.clear();
-		myWords = text.split("\\s+");
-		int len=myWords.length;
-		for (int k=0; k< len - myOrder; k++) {
-			WordGram myMapKey=new WordGram(myWords,k,myOrder);
-			if (!myMap.containsKey(myMapKey)) {
-				ArrayList <String> MyMapValue= new ArrayList <String>();
-				MyMapValue.add(myWords[k+ myOrder]);
-				myMap.put(myMapKey, MyMapValue);
-			}else 
-				{myMap.get(myMapKey).add(myWords[k+ myOrder]);}	
-		}
+		myText=text;
+		int len=text.length();
 		
-		WordGram MyMapKey = new WordGram(myWords, len - myOrder, myOrder);
+		for (int k=0; k< len - myOrder; k++) {
+			String MyMapKey= text.substring(k, k + myOrder);
+			if (!myMap.containsKey(MyMapKey)) {
+				ArrayList <String> MyMapValue= new ArrayList <String>();
+				MyMapValue.add(text.substring(k+myOrder, k+myOrder +1));
+				myMap.put(MyMapKey, MyMapValue);
+			}else 
+				{myMap.get(MyMapKey).add(text.substring(k+myOrder ,k+myOrder +1));}
+		}
+		String MyMapKey= text.substring(len-myOrder, len);
 		if (!myMap.containsKey(MyMapKey)) {
 			ArrayList<String> MyMapValue = new ArrayList<String>();
 			MyMapValue.add(PSEUDO_EOS);
 			myMap.put(MyMapKey, MyMapValue);
-			
 		} else {
 			myMap.get(MyMapKey).add(PSEUDO_EOS);
 		}
-		
+
 	}
+	
 	/**
 	 * Returns the value of a given key in myMap and throws a No Such Element Exception if the key isn't found
-	 * @param a given Wordgram
+	 * @param a given string
 	 * @return An array list of strings
 	 */
+	
 	@Override
-		public ArrayList<String> getFollows(WordGram key) {
-			if (!myMap.containsKey(key)) {
-				throw new NoSuchElementException();
-				
-			} else {
-				return myMap.get(key); 
-			}
+	public ArrayList<String> getFollows(String MyMapKey) {
+		if (!myMap.containsKey(MyMapKey)) {
+			throw new NoSuchElementException(MyMapKey + " not in map");
+		} else {
+			return myMap.get(MyMapKey);
 		}
-	
-	
+	}
+
 }
